82fe9af94eb90aff6956d46deffc78afc3b2ee64
Travis Valenti <Bizarzoo10@ymail.com>
2015-04-16 14:47:17 +1000

    Network Managing Prototupe
    ><
    `````

---
+++ New Unity Project/Assets/C#/networkManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class networkManager : MonoBehaviour {
+	//Starts Server
+	public void startServer(int maxConnect, int port, bool bLAN) {
+		Network.InitializeServer(maxConnect, port, bLAN);
+	}
+	//Joins Server
+	public void joinServer(string ip, int port, string pass) {
+		Network.Connect(ip, port, pass);
+	}
+	void OnServerInitialized(){
+		Debug.Log("Server Initialized.");
+	}
+	void OnConnectedToServer(){
+		Debug.Log("Connected to Server");
+	}
+	void OnPlayerDisconnected(NetworkPlayer player) {
+		if(Network.isServer){
+			Network.RemoveRPCs(player);
+			Network.DestroyPlayerObjects(player);
+		}
+		Debug.Log ("Player has left");
+	}
+}
+
